2ea84feea4df9ffd7b3cab684fd92847a0c31ead
Marijn van den Bos <marijn.vandenbos@iodigital.com>
2025-04-01 13:56:00 +0200

    Reworked LastPayment and Meta
    I reworked the last payment and meta object from the dictionary to the list, as it is a JSON list in the response.

---
+++ src/Twikey/Model/Parameters/LastPayment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Twikey.Model.Parameters;
+
+public class LastPayment
+{
+    [JsonProperty("method", NullValueHandling = NullValueHandling.Ignore)]
+    public string PaymentMethod { get; set; }
+    
+    [JsonProperty("action", NullValueHandling = NullValueHandling.Ignore)]
+    public string Action { get; set; }
+    
+    [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+    public int Id { get; set; }
+    
+    [JsonProperty("e2e", NullValueHandling = NullValueHandling.Ignore)]
+    public string EndToEndReference { get; set; }
+    
+    [JsonProperty("pmtinf", NullValueHandling = NullValueHandling.Ignore)]
+    public string PaymentInformation { get; set; }
+    
+    [JsonProperty("iban", NullValueHandling = NullValueHandling.Ignore)]
+    public string Iban { get; set; }
+    
+    [JsonProperty("bic", NullValueHandling = NullValueHandling.Ignore)]
+    public string Bic { get; set; }
+    
+    [JsonProperty("rc", NullValueHandling = NullValueHandling.Ignore)]
+    public string ReturnCode { get; set; }
+    
+    [JsonProperty("link", NullValueHandling = NullValueHandling.Ignore)]
+    public string PaymentLink { get; set; }
+    
+    [JsonProperty("msg", NullValueHandling = NullValueHandling.Ignore)]
+    public string Message { get; set; }
+    
+    [JsonProperty("mndtId", NullValueHandling = NullValueHandling.Ignore)]
+    public string MandateId { get; set; }
+    
+    [JsonProperty("date", NullValueHandling = NullValueHandling.Ignore)]
+    public DateTime TransactionDate { get; set; }
+    
+}
---
+++ src/Twikey/Model/Parameters/Meta.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Twikey.Model.Parameters;
+
+public class Meta
+{
+    [JsonProperty("reminderLevel", NullValueHandling = NullValueHandling.Ignore)]
+    public int ReminderLevel { get; set; }
+    
+    [JsonProperty("partial", NullValueHandling = NullValueHandling.Ignore)]
+    public string Partial { get; set; }
+    
+    [JsonProperty("lastError", NullValueHandling = NullValueHandling.Ignore)]
+    public string LastError { get; set; }
+    
+    [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+    public string Name { get; set; }
+    
+    [JsonProperty("paymentMethod", NullValueHandling = NullValueHandling.Ignore)]
+    public string PaymentMethod { get; set; }
+}
