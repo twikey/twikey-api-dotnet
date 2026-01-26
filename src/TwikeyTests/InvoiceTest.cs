85150cb2dd7700ed41e25e782b917021a896009f
ThalesTomme <thales.tomme@twikey.com>
2026-01-26 17:09:54 +0100

    Added pdf fetch for invoices

--- src/TwikeyTests/InvoiceTest.cs
+++ src/TwikeyTests/InvoiceTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 using Twikey;
 using Twikey.Model;
 using System.Threading.Tasks;
@@ -192,6 +193,64 @@ namespace TwikeyAPITests
                 Assert.IsNotNull(payment);
                 Assert.IsFalse(string.IsNullOrEmpty(payment.EventId));
             }
+        }
+
+        [TestMethod]
+        public void GetInvoicePdf()
+        {
+            if (_apiKey == null)
+            {
+                Assert.Inconclusive("apiKey is null");
+                return;
+            }
+
+            var invoice = new Invoice()
+            {
+                Number = "InvoicePdf" + DateTime.Now.ToString("yyyyMMddHHmmss"),
+                Title = "Invoice with PDF",
+                Remittance = s_testVersion,
+                Amount = 10.90,
+                Date = DateTime.Now,
+                Duedate = DateTime.Now.AddDays(30),
+                Pdf = File.ReadAllBytes("TestResources/empty.pdf")
+            };
+
+            invoice = _api.Invoice.Create(_ct, _customer, invoice);
+            Assert.IsNotNull(invoice);
+            Assert.IsFalse(string.IsNullOrEmpty(invoice.Id));
+
+            var pdfStream = _api.Invoice.Pdf(invoice.Id);
+            Assert.IsNotNull(pdfStream);
+            Assert.IsTrue(pdfStream.Length > 0);
+        }
+
+        [TestMethod]
+        public async Task AsyncGetInvoicePdf()
+        {
+            if (_apiKey == null)
+            {
+                Assert.Inconclusive("apiKey is null");
+                return;
+            }
+
+            var invoice = new Invoice()
+            {
+                Number = "InvoicePdf" + DateTime.Now.ToString("yyyyMMddHHmmss"),
+                Title = "Invoice with PDF",
+                Remittance = s_testVersion,
+                Amount = 10.90,
+                Date = DateTime.Now,
+                Duedate = DateTime.Now.AddDays(30),
+                Pdf = File.ReadAllBytes("TestResources/empty.pdf")
+            };
+
+            invoice = await _api.Invoice.CreateAsync(_ct, _customer, invoice);
+            Assert.IsNotNull(invoice);
+            Assert.IsFalse(string.IsNullOrEmpty(invoice.Id));
+
+            var pdfStream = await _api.Invoice.PdfAsync(invoice.Id);
+            Assert.IsNotNull(pdfStream);
+            Assert.IsTrue(pdfStream.Length > 0);
         }
     }
 }
